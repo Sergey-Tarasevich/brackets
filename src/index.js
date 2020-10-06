44e7473a07186e78febee3893961abfa0c000952
Sergey Tarasevich <70640645+Sergey-Tarasevich@users.noreply.github.com>
2020-10-06 11:53:55 +0300

    feat: add solution task brackets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-    // your solution
+    let result = [];
 
+    for (let i = 0; i < bracketsConfig.length; i++) {
+
+        result.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str.includes(result[i])) {
+
+            str = str.replace(result[i], "")
+            i = -1
+        }
+    }
+
+    if (str.length == 0) {
+        return true
+
+    } else {
+
+        return false
+    }
 }
-
